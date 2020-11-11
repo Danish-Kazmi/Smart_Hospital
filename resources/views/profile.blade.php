d61b7e851c142b16ce18b05ebdb5417f45928070
Danish-Kazmi <s.danish.kazmi555@gmail.com>
2020-11-11 17:00:56 +0500

    PHP Verision Changed

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -21,15 +21,6 @@ $email = $user->email;
 $tp = $user->contactnumber;
 $id = $user->id;
 
-
-
-
-// "require": {
-//                 "php": ">=5.3.3",
-//                 "phpseclib/bcmath_compat": ">=1.0.3"
-//             },
-
-
 $outlet = 'Rural Ayruvedic Hospital Kesbawa'?>
 
 <section class="content">
