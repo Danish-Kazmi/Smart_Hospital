c0194499f887f15362eccbcde52449b8ddc20bed
Danish-Kazmi <s.danish.kazmi555@gmail.com>
2020-11-13 15:20:10 +0500

    Activity Updated

--- database/migrations/2019_09_16_075323_create_activity_log_table.php
+++ database/migrations/2019_09_16_075323_create_activity_log_table.php
@@ -14,13 +14,13 @@ class CreateActivityLogTable extends Migration
         Schema::create(config('activitylog.table_name'), function (Blueprint $table) {
             $table->increments('id');
             $table->string('log_name')->nullable();
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->integer('subject_id')->nullable();
             $table->string('subject_type')->nullable();
             $table->integer('causer_id')->nullable();
             $table->string('causer_type')->nullable();
             $table->text('properties')->nullable();
-            $table->timestamps()->nullable();
+            $table->timestamps();
 
             $table->index('log_name');
         });
